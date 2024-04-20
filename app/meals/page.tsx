377c230da0cacfc181016c0f292956d31b30616d
AHMED-SHALABY\ahmed <ahmed_a.mahmoud@hotmail.com>
2024-04-20 16:19:48 +0200

    feat(reponsive): Improve reponsiveness for mobile

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -24,7 +24,7 @@ const MealsPage = () => {
       <header className={classes.header}>
         <h1>
           Delicious meals, created
-          <span className={classes.highlight}>by you</span>
+          <span className={classes.highlight}> by you</span>
         </h1>
         <p>
           Choose your favorite recipe and cook it yourself. It is easy and fun!
